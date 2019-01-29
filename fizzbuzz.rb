ed1138a61754fdc2f1f74d33fb95dd486c0eaa4f
Kirsten Werner <kirstenmwerner@gmail.com>
2019-01-29 01:54:45 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,10 @@
 def fizzbuzz(num)
-  if num % 3 == 0 
+  if (num % 3 == 0) && (num % 5 == 0)
+    "FizzBuzz"
+  elsif num % 3 == 0 
     "Fizz"
   elsif num % 5 == 0
     "Buzz"
+  else
   end
 end
